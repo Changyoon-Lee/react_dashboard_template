cf4ab13a08a90102ffc60539e8533cb13db3e1c1
Changyoon-Lee <lkingkongl@naver.com>
2023-04-06 15:46:16 +0900

    tailwind darkmode config 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class', // class를 활용하여 darkmode 적용을 위함
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
