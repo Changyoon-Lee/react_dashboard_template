0ece21bf1195bf3953a4bd39e4fb979ae4b7cdbd
Changyoon-Lee <lkingkongl@naver.com>
2023-04-11 19:26:59 +0900

    크기조절 기능 완료

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,23 +1,40 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import styles from "./ContentBox.module.css";
+import { BoxState } from "./DashBoard";
 interface ContentBoxProps {
   id: number;
   index: number;
+  gridRowSpan: number;
+  gridColSpan: number;
   moveBox: (boxId: number, toIndex: number) => void;
 }
 /**
  * Your Component
  */
-export default function ContentBox({ id, index, moveBox }: ContentBoxProps) {
-  const [{ isDragging, handlerId }, connectDrag, dragPreview] = useDrag(
+export default function ContentBox({
+  id,
+  index,
+  gridRowSpan,
+  gridColSpan,
+  moveBox,
+}: ContentBoxProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [width, setWidth] = useState(0);
+  const [hieght, setHiehgt] = useState(0);
+  useEffect(() => {
+    setWidth(ref.current ? ref.current.offsetWidth : 0);
+    setHiehgt(ref.current ? ref.current.offsetHeight : 0);
+    console.log(width, hieght);
+  }, [ref.current, gridRowSpan, gridColSpan]);
+  const [{ isDragging }, connectDrag, dragPreview] = useDrag(
     // dragPreview: 드래그 도중에 보여줄 화면
     () => ({
       type: "Box", //It is used by the "accept" specification of drop targets.
       item: { id, index }, // item에 선언되는 것은 drop tartet에서 알수 있는 drag source 정보이다
       collect: (monitor) => ({
         isDragging: monitor.isDragging(), // isDragging 변수가 현재 드래깅중인지 아닌지를 기록
-        handlerId: monitor.getHandlerId(),
       }),
       end: (item, monitor) => {
         //드래그가 끝났을 때 작동하는 부분,
@@ -56,22 +73,54 @@ export default function ContentBox({ id, index, moveBox }: ContentBoxProps) {
     [moveBox]
   );
 
+  const [{ isResizing }, connectResize] = useDrag(
+    // dragPreview: 드래그 도중에 보여줄 화면
+    () => ({
+      type: "Resize", //It is used by the "accept" specification of drop targets.
+      item: { id, gridRowSpan, gridColSpan, width, hieght }, // item에 선언되는 것은 drop tartet에서 알수 있는 drag source 정보이다
+      collect: (monitor) => ({
+        isResizing: monitor.isDragging(), // isDragging
+      }),
+      end: (item, monitor) => {
+        //드래그가 끝났을 때 작동하는 부분,
+        const { gridRowSpan: originRow, gridColSpan: originCol } = item; // box를 내려놓을때 먼저 초기 id,index를 받아온다
+        const didDrop = monitor.didDrop(); // check whether or not the drop was handled by a compatible drop target.
+        if (!didDrop) {
+          //connectDrop으로 선언한 태그위에 드랍되지 않았을때
+          console.log("크기변경실패");
+        }
+      },
+    }),
+    [width, hieght]
+  );
+
+  connectDrag(connectDrop(ref));
   return (
     /* This is optional. The dragPreview will be attached to the dragSource by default */
     <div
-      ref={connectDrop}
-      style={{ opacity: isDragging ? 0.5 : 1 }}
-      className={`relative flex flex-col column row-span-${3} col-span-${3} bg-gray-300 rounded-xl shadow-md overflow-hidden`} //{styles.box} //
+      ref={ref}
+      className={`relative flex flex-col column row-span-${gridRowSpan} col-span-${gridColSpan} bg-gray-300 rounded-lg shadow-md overflow-hidden`} //{styles.box} //
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        gridColumn: `auto/span ${gridColSpan}`,
+        gridRow: `auto/span ${gridRowSpan}`,
+      }}
     >
-      <div
-        ref={connectDrag}
-        className="bg-blue-500"
-        data-handler-id={handlerId}
-      >
-        handler id 는 {handlerId?.toString()}, id는 {id} 입니다
+      <div className="bg-blue-500">
+        <span>id는 {id} 입니다</span>
+        <span>
+          width: {width} height: {hieght}
+        </span>
       </div>
       <div className="bg-red-200 opacity-50 w-10 h-10"></div>
-      <div>index는 {index}</div>
+      <div>
+        index는 {index}{" "}
+        {isResizing ? `${gridRowSpan || 3} x ${gridColSpan || 3}` : ""}
+      </div>
+      <div
+        className="absolute bottom-0 right-0 w-0 h-0 border-[10px] border-black border-t-transparent border-l-transparent cursor-nw-resize"
+        ref={connectResize}
+      ></div>
     </div> //The drag ref marks this node as being the "pick-up" node
   );
 }
--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,12 +1,20 @@
 import ContentBox from "./ContentBox";
 import { useState } from "react";
 import styles from "./DashBoard.module.css";
+import { useDrop } from "react-dnd";
 interface DashBoardProps {
   children?: React.ReactNode;
 }
-
+export interface BoxState {
+  row?: number;
+  col?: number;
+}
+interface BoxStates {
+  [id: number]: BoxState;
+}
 function DashBoard({ children }: DashBoardProps) {
   const [order, setOrder] = useState([10, 11, 12, 13, 14, 15, 16, 17]); //각 자리는 index고 해당 index의 id값은 value 임
+  const [boxStates, setBoxStates] = useState<BoxStates>({}); // key : id , value : {col, row}
   const moveBox = (boxId: number, toIndex: number): void => {
     //boxId 를 toIndex로 이동
     const preIndex = order.indexOf(boxId); // boxId의 기존인덱스
@@ -19,13 +27,69 @@ function DashBoard({ children }: DashBoardProps) {
     setOrder(newOrder); // order를 새로 업데이트
   };
 
-  console.log(`order는 ${order}`);
+  const [, connectResizeDrop] = useDrop(() => ({
+    accept: "Resize",
+    //   canDrop: () => false, // 이동 안되는 조건설정하고 싶을때 여기다 설정
+    hover: (
+      {
+        //인풋은 드래그하고있는 오브젝트정보
+        id: id,
+        gridRowSpan: currentRowSize,
+        gridColSpan: currentColSize,
+        width,
+        hieght,
+      }: {
+        id: number;
+        gridRowSpan: number;
+        gridColSpan: number;
+        width: number;
+        hieght: number;
+      },
+      monitor
+    ): void => {
+      const XYcoord = monitor.getDifferenceFromInitialOffset();
+
+      const { x, y } = XYcoord || { x: 0, y: 0 };
+      // console.log(width, hieght);
+      const thresX = width / currentColSize;
+      const thresY = hieght / currentRowSize;
+      //   console.log(thresX, thresY);
+      if (Math.abs(x) > thresX || Math.abs(y) > thresY) {
+        currentColSize += Math.round(x / thresX);
+        currentRowSize += Math.round(y / thresY);
+
+        // let newState = { ...boxStates };
+        // newState[id] = {
+        //   row: currentRowSize > 3 ? currentRowSize : 3,
+        //   col: currentColSize > 3 ? currentColSize : 3,
+        // };
+        setBoxStates((prevState) => {
+          let newState = { ...prevState };
+          newState[id] = {
+            row: currentRowSize > 3 ? currentRowSize : 3,
+            col: currentColSize > 3 ? currentColSize : 3,
+          };
+          return newState;
+        });
+      }
+    },
+  }));
+
+  //   console.log(`order는 ${order}`);
   return (
     <div className="p-5 space-y-5">
       <div>{order.join(" ")}</div>
-      <div className={styles.grid}>
+      <div>{JSON.stringify(boxStates)}</div>
+      <div className={styles.grid} ref={connectResizeDrop}>
         {order.map((i, index) => (
-          <ContentBox id={i} index={index} moveBox={moveBox} key={i} />
+          <ContentBox
+            id={i}
+            index={index}
+            gridRowSpan={boxStates[i]?.row || 3}
+            gridColSpan={boxStates[i]?.col || 3}
+            moveBox={moveBox}
+            key={i}
+          />
         ))}
       </div>
     </div>
